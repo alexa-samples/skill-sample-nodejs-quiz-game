e6b53ae8992736b2960c6fb436bb3b277a6e4797
Jake Kelly <jakemkelly84@gmail.com>
2018-09-28 11:31:54 -0600

    Fix a typo

--- lambda/custom/languages/en.js
+++ lambda/custom/languages/en.js
@@ -8,7 +8,7 @@ module.exports = {
     START_QUIZ_MESSAGE: `OK.  I will ask you 10 questions about the United States. `,
     GET_SPEECH_DESCRIPTION: `%s is the %sth state, admitted to the Union in %s.  The capital of %s is %s, and the abbreviation for %s is <break strength='strong'/><say-as interpret-as='spell-out'>%s</say-as>.  I've added %s to your Alexa app.  Which other state or capital would you like to know about?`,
     GET_QUESTION: `Here is your %sth question.  What is the %s of %s?`,
-    GET_QUESTION_WITHOUT_ORDINAL; `What is the %s of %s?`,
+    GET_QUESTION_WITHOUT_ORDINAL: `What is the %s of %s?`,
     EXIT_SKILL_MESSAGE: `Thank you for playing the United States Quiz Game!  Let's play again soon!`,
     GET_BAD_ANSWER: `I'm sorry. %s is not something I know very much about in this skill.`,
     GET_ANSWER: `The %s of %s is %s. `,
